b5a47eb5fd3b4d2cc27a57642050bcbc90417c15
Dima Kuz <kuzdima2@gmail.com>
2020-08-10 23:58:40 +0500

    Поменять переменные местами

---
+++ 1/var_change.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int num = 4;
+  int values[num];
+
+  printf("Enter %d integers: \n", num);
+
+  for(int i = 0; i < num; ++i) {
+     scanf("%d", &values[i]);
+  }
+
+  printf("Displaying Variable: \n");
+
+char name_values[] = {'a', 'b', 'c', 'd'};
+  for(int i = 0; i < num; ++i) {
+     printf("Variable is %c and value %d\n", name_values[i], values[i]);
+  }
+
+  printf("Displaying Change Variable: \n");
+
+   for (int count = num - 1; count >= 0; count--)  {
+     int antivar = num - 4; //считаем в обратную сторону для массива с именами переменных
+     int n = abs(antivar);
+     printf("Variable change is %c and value %d\n", name_values[n], values[count]);
+     num--;
+  }
+
+
+  return 0;
+}
