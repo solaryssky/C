7e2510b108b8616ffe3f138f8e6b8ba403ad4347
solaryssky <kuzdima2@gmail.com>
2021-02-12 00:38:46 +0500

    Update info.c
    добавлено более читаемое условие сравнения строк и вывод used space целым числом

--- diskinfo/info.c
+++ diskinfo/info.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {   
  
  if(argc < 2) {
-    printf("Not set arguments. Exiting the program....\n");
+    printf("Not set arguments. Example: used -[u|t|a] <mount point>\n");
     exit(EXIT_SUCCESS);
     }
 	
@@ -33,16 +33,17 @@ int main(int argc, char *argv[])
         const double used = total - available;
         const double usedPercentage = (double)(used / total) * (double)100;
         
-		if (!strcmp("-t", keys)) {
+		if (strcmp("-t", keys) == 0) {
 		printf("Total: %f --> %.0f\n", total, total);
 		}
-		else if (!strcmp("-a", keys)) {
+		else if (strcmp("-a", keys) == 0) {
         printf("Available: %f --> %.0f\n", available, available);
 		}
-		else if (!strcmp("-u", keys)) {
-        printf("Used: %f --> %.1f\n", used, used);
+		else if (strcmp("-u", keys) == 0) {
+        //printf("Used: %f --> %.1f\n", used, used);
+		printf("%.0f\n", used);
 		}
-		else if (!strcmp("-p", keys)) {
+		else if (strcmp("-p", keys) == 0) {
         printf("Used Percentage: %f --> %.0f\n", usedPercentage, usedPercentage);
 		}
     }
